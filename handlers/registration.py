a06aee601a01d4b1084d58cf489a5f098604933c
vot3tada <zindromed@bk.ru>
2023-03-24 11:47:14 +0300

    do cancel registration add some shit messages ban repeated registration

--- handlers/registration.py
+++ handlers/registration.py
@@ -8,17 +8,35 @@ users = dict()
 
 class FSMRegistation(StatesGroup):
     name = State()
+    photoclass = State()
     photo = State()
 
 async def reg_start(message : types.Message):
+    if str(message.from_user.id) in users.keys():
+        await message.reply('Ты уже зареган(а)')
+        return
     await FSMRegistation.name.set()
     await message.reply('Напиши имя')
 
 async def get_name(message : types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
+    await FSMRegistation.photoclass.set()
+    text = '\n'
+    for i in ac.classes.keys():
+        text += i + '\n'
+    await message.reply('Выбери класс фото:' + text)
+
+
+async def get_photoclass(message : types.Message, state: FSMContext):
+    if (str. lower(message.text) not in ac.classes.keys()):
+        await message.reply('Неправильный класс')
+        return
+    async with state.proxy() as data:
+        data['photoclass'] = message.text
     await FSMRegistation.photo.set()
     await message.reply('Добавь фото')
+
 
 async def end_registation(message : types.Message, state: FSMContext):
     orig = f'./static/{message.from_user.id}.jpg'
@@ -27,11 +45,14 @@ async def end_registation(message : types.Message, state: FSMContext):
         ac.getAvatar(orig)
     except:
         await message.reply('Плохое фото, попробуй ещё раз')
-        return
+        return 
     async with state.proxy() as data:
         users[str(message.from_user.id)] = data['name'] 
     await state.reset_state(with_data=False)
-    await message.reply('Регистрация завершена')
+    orig = f'./static/{message.from_user.id}.jpg'
+    photo=open(orig, "rb")
+    await message.answer_photo(photo, caption='Ещё один красавчик/одна чикуля с нами: ' + users[str(message.from_user.id)] + '!!')
+
 
 async def get_avatar(message : types, state: FSMContext):
     orig = f'./static/{message.from_user.id}.jpg'
@@ -40,10 +61,18 @@ async def get_avatar(message : types, state: FSMContext):
     async with state.proxy() as data:
         text = data['name']
     await message.answer_photo(photo, caption=text)
+
+async def cancel_registration(message: types, state: FSMContext):
+    await state.finish()
+    await message.reply('Регистрация отменена')
     
 def register_handlers_registration(dp: Dispatcher):
     dp.register_message_handler(reg_start, regexp='^Регистрация$', state=None)
+    dp.register_message_handler(cancel_registration, regexp='^Отмена$', state=[FSMRegistation.name, FSMRegistation.photo])
     dp.register_message_handler(get_avatar, regexp='^Аватар$', state=None)
+    
     dp.register_message_handler(get_name, state=FSMRegistation.name)
+    dp.register_message_handler(get_photoclass, state=FSMRegistation.photoclass)
     dp.register_message_handler(end_registation, content_types=['photo'], state=FSMRegistation.photo)
+    
     
