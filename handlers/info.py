9af0189550d10649a6aefb7d9fb7ac9c1505f08f
sceletoniK <L.s1n56@yandex.ru>
2023-04-10 16:04:30 +0300

    added start command

--- handlers/info.py
+++ handlers/info.py
@@ -1,6 +1,33 @@
 from aiogram.dispatcher import Dispatcher
+from emoji import emojize
 from aiogram import types
 import os, random
+
+async def welcomeMessage(message: types.Message):
+
+    text = f'''<b>Приветсвую тебя!</b>
+
+Я - <b>Омехалюль</b>, игровой бот в стиле нейронного стимпанка
+
+Добавляй меня в свой чат к друзьям, давай админку и вместе вы сможете:
+
+{emojize(':man_construction_worker:')} <i>Создать своего аватара-инженера</i>. Прифотошопьте свое лицо с заранее заготовленным инженерам и радуйте свой глаз <i>великолепным</i> дипфейсом.
+
+{emojize(':crossed_swords:')} <i>Устраивать дуэли с другими инженерами</i>. Ультуйте, защищайтесь и обыгрывайте своего противника за награды.
+
+{emojize(':man_lifting_weights:')} <i>Выполнять задания других инженеров и создавать свои</i>. Словно ведьмак или авантюрист вы будете выполнять задания даже из реального мира.
+
+{emojize(':office_building:')} <i>Ходить на работу</i>. Пока вы сами батрачите отправьте своего инженера работать тоже, чтобы неповадно было.
+
+{emojize(':party_popper:')}  <i>Организовывать мероприятия</i>. Перенесите совещания или сбор в курилке прямо сюда и получите награды и напоминалки о собраниях.
+
+{emojize(':sports_medal:')} <i>Участвовать в квизах</i>. Быстрее других отвечайте на вопросы и получайте награду как чемпион зверей и гиков.
+
+{emojize(':ogre:')} <i>Собирать рейды на босса</i>. Объединяйтесь в другими инженерами для того чтобы повалить ежедневного босса качалки.
+
+{emojize(':mechanical_arm:')} Прокачивайтесь, тратьте деньги на расходники, получайте амуницию с боссов и получайте ачивки. Станьте инженером мессяца!'''
+
+    await message.answer(text=text, parse_mode='HTML')
 
 async def getCommands(message: types.Message):
     await message.reply_photo(
@@ -35,3 +62,4 @@ async def getCommands(message: types.Message):
 
 def register_handlers_info(dp: Dispatcher):
     dp.register_message_handler(getCommands, commands='help', state='*')
+    dp.register_message_handler(welcomeMessage, commands='start', state=None)
