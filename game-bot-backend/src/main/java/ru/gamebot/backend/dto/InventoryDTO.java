1cdf5a6b262c78bfda525cbd19a5f818f2854f61
IB133 <bii.20@uni-dubna.ru>
2023-04-13 18:08:30 +0300

    fix: deserialize problems

--- game-bot-backend/src/main/java/ru/gamebot/backend/dto/InventoryDTO.java
+++ game-bot-backend/src/main/java/ru/gamebot/backend/dto/InventoryDTO.java
@@ -1,8 +1,10 @@
 package ru.gamebot.backend.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class InventoryDTO {
     private Integer itemId;
     private Integer count;
